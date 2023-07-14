1469dbd0501e6de5b8de26f441372b4c4efc9fc3
Roman Makudera <roman.makudera@firma.seznam.cz>
2023-07-14 12:55:34 +0200

    aktualizace verze

--- update-version.js
+++ update-version.js
@@ -4,6 +4,13 @@ const fs = require("fs");
 const version = PACKAGE.version.toString();
 const parts = version.split(".").map(item => parseFloat(item));
 parts[2]++;
+
+if (parts[2] >= 10) {
+	parts[1]++;
+	parts[2] = 0;
+}
+
 const newVersion = parts.join(".");
 PACKAGE.version = newVersion;
+console.log(`Update version ${version} -> ${newVersion}`);
 fs.writeFileSync(file, JSON.stringify(PACKAGE, null, "\t"), "utf-8");
